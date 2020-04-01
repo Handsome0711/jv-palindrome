c1ec148f6973ed5738594ff7a0dbf420cbedc2e7
vladyslav <vlad.bes0711@gmail.com>
2020-04-01 16:29:53 +0300

    shifted the lines

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 public class Palindrome {
     public boolean isPalindrome(String text) {
-        StringBuilder resultText = new StringBuilder(text.toLowerCase().replaceAll("[^A-Za-zА-Яа-я0-9]", ""));
+        StringBuilder resultText = new StringBuilder(text
+                .toLowerCase()
+                .replaceAll("[^A-Za-zА-Яа-я0-9]", ""));
         return resultText.toString().equals(resultText.reverse().toString());
     }
 }
